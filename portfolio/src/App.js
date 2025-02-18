f2a474285ff17dffcb8895c03ab830d0d809d9b8
Kayo Osako <osakok@cardiff.ac.uk>
2025-02-18 19:06:47 +0000

    edit App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,44 @@
+import React from "react";
 import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import Header from "./components/Header";
+import About from "./components/About";
+import Works from "./components/Works";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Header/>
+        <main>
+          <section id="top">
+            <h1><span className="highlight-font">Hello, I am </span><br/> Kayo OSAKO</h1>
+            <p>I am an aspiring software engineer with over 5 years of experience in the IT industry in Japan, including
+              more than 2 years as a software development product manager.
+            </p>
+            <div className="links">
+              <a href="https://github.com/ko-sako">
+                <img src="assets/github.png" alt="Link to Kayo's Github repo"/>
+              </a>
+              <a href="https://www.linkedin.com/in/kayo-osako-0b4699170/">
+                <img src="assets/linkedin.png" alt="Link to Kayo's LinkedIn page"/>
+              </a>
+              <a href="mailto:lapin.587k@gmail.com">
+                <img src="assets/mail.png" alt="Send email to Kayo"/>
+              </a>
+              <a href="assets/2401021CV_KayoOsako-open.pdf" download>
+                <img src="assets/cv.png" alt="Download Kayo's CV"/>
+              </a>
+            </div>
+          </section>
+          <About/>
+          <Works/>
+          <Contact/>
+        </main>
+        <footer>
+          <p>© Copyright 2024 All rights reserved.</p>
+        </footer>
+      </div>
   );
 }
 
