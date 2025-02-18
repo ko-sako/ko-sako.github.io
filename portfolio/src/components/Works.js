673199dd2e01aca1a84270d6d06bb4b83b8aab4d
Kayo Osako <osakok@cardiff.ac.uk>
2025-02-18 18:57:32 +0000

    create Works.js and move from index.html

---
+++ portfolio/src/components/Works.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Works = () => {
+    return (
+        <section id="works">
+            <h2>Works</h2>
+            <div class="work-items">
+                <a href="#">
+                    <div class="work-title text"><h3>Portfolio Site</h3></div>
+                    <div class="text small">HTML, CSS</div>
+                </a>
+                <a href="#">
+                    <div class="work-title text"><h3>Portfolio Site</h3></div>
+                    <div class="text small">HTML, CSS</div>
+                </a>
+                <a href="#">
+                    <div class="work-title text"><h3>Portfolio Site</h3></div>
+                    <div class="text small">HTML, CSS</div>
+                </a>
+            </div>
+        </section>
+    );
+};
+
+export default Works;
