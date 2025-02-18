f685fa4768b467b81dcf6dc46f3d631eb7660096
Kayo Osako <osakok@cardiff.ac.uk>
2025-02-18 21:18:55 +0000

    add br tag appropriately

--- portfolio/src/components/Works.js
+++ portfolio/src/components/Works.js
@@ -4,18 +4,21 @@ const Works = () => {
     return (
         <section id="works">
             <h2>Works</h2>
-            <div class="work-items">
+            <div className="work-items">
                 <a href="#">
-                    <div class="work-title text"><h3>Portfolio Site</h3></div>
-                    <div class="text small">HTML, CSS</div>
+                    <div className="work-title text"><h3>Portfolio Site</h3></div>
+                    <br></br>
+                    <div className="text small">HTML, CSS</div>
                 </a>
                 <a href="#">
                     <div className="work-title text"><h3>Portfolio Site</h3></div>
-                    <div class="text small">HTML, CSS</div>
+                    <br></br>
+                    <div className="text small">HTML, CSS</div>
                 </a>
                 <a href="#">
-                    <div class="work-title text"><h3>Portfolio Site</h3></div>
-                    <div class="text small">HTML, CSS</div>
+                    <div className="work-title text"><h3>Portfolio Site</h3></div>
+                    <br></br>
+                    <div className="text small">HTML, CSS</div>
                 </a>
             </div>
         </section>
